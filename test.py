64063e5d4f83caa1799644ecd4e5a67b8cf84c5c
Hao Yunfei <yunfei.hao@intel.com>
2017-06-29 22:41:27 +0800

    try commit

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ chrome_options.binary_location = 'C:\Users\test\Desktop\chromium_webvr1.1_201706
 driver = webdriver.Chrome(executable_path='C:\Users\haoyunfe\Documents\chromedriver.exe', chrome_options=chrome_options)  # Optional argument, if not specified will search path.
 driver.get('http://wp-27.sh.intel.com/workspace/project/readonly/webbench/webgl/webglsamples/aquarium/aquarium-mod.html');
 time.sleep(5) # Let the user actually see something!
+
+
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
