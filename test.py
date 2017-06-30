a80bfebb3c9cce2af9b9e0947adfb527de757aa2
Hao Yunfei <yunfei.hao@intel.com>
2017-06-30 10:17:29 +0800

    commit try from windows remote

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ driver = webdriver.Chrome(executable_path='C:\Users\haoyunfe\Documents\chromedri
 driver.get('http://wp-27.sh.intel.com/workspace/project/readonly/webbench/webgl/webglsamples/aquarium/aquarium-mod.html');
 time.sleep(5) # Let the user actually see something!
 
-
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
