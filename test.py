ac33ed523747c1aa09b641603334cf9887a82f87
Hao Yunfei <yunfei.hao@intel.com>
2017-07-02 22:25:41 +0800

    work on 20170702

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-chrome_options = webdriver.ChromeOptions
-chrome_options.binary_location = 'C:\Users\test\Desktop\chromium_webvr1.1_20170628\chrome.exe'
-driver = webdriver.Chrome(executable_path='C:\Users\haoyunfe\Documents\chromedriver.exe', chrome_options=chrome_options)  # Optional argument, if not specified will search path.
+# enable browser logging
+d = DesiredCapabilities.CHROME
+d['loggingPrefs'] = { 'browser':'ALL' }
+
+#chrome_options = webdriver.ChromeOptions
+webdriver.chrome.options.binary_location = r'C:\Users\test\Desktop\chromium_webvr1.1_20170628\chrome.exe'
+
+#driver = webdriver.Chrome(executable_path=r'C:\dev\chromedriver\2.29\chromedriver.exe', chrome_options=chrome_options, desired_capabilities=d)
+driver = webdriver.Chrome(executable_path=r'C:\dev\chromedriver\2.29\chromedriver.exe', desired_capabilities=d)
 driver.get('http://wp-27.sh.intel.com/workspace/project/readonly/webbench/webgl/webglsamples/aquarium/aquarium-mod.html');
+#for entry in driver.get_log('browser'):
+#    print entry
 time.sleep(5) # Let the user actually see something!
+while True:
+    fps = driver.find_element_by_id('fps').get_attribute('innerText')
+    print fps
+    time.sleep(1)
 
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+#time.sleep(5) # Let the user actually see something!
+#driver.quit()
